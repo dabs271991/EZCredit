1d508e8bc062ca7d5ab0d07169c2892cd4f40ed5
ALONSO BRENES SANCHEZ DIEGO <200933156@estudiantec.cr>
2020-09-14 23:18:26 -0600

    Agregando base de datos

---
+++ EZCredit/dbo/Tables/Cliente.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Cliente] (
+    [id]         INT          NOT NULL,
+    [Nombre]     VARCHAR (50) NOT NULL,
+    [Contraseña] VARCHAR (50) NOT NULL DEFAULT 12345,
+    [Codigo]   INT          NOT NULL, 
+    [Direccion] VARCHAR(50) NULL, 
+    [Telefono] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_Cliente] PRIMARY KEY ([id]), 
+    
+);
+
---
+++ EZCredit/dbo/Tables/Tarjeta.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Codigo] INT NOT NULL, 
+    [Tipo] VARCHAR(50) NOT NULL, 
+    [Numero] INT NOT NULL, 
+    [Vencimiento] DATE NOT NULL, 
+    [CVV] INT NOT NULL
+)
